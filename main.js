e8a1399d916bbb9f4d598c885ff5f996594aad32
Daniel-spec16 <80032117+Daniel-spec16@users.noreply.github.com>
2022-06-29 00:02:19 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const projectEyes = document.getElementById("Eyes");
 const projectPacman = document.getElementById("PacMan");
 const projectBus = document.getElementById("BusStops");
 
-const descriptionEyes = "";
-const descriptionBus = "Bla Bla Bus";
+const descriptionEyes = "Eye movement is a web page with two elements which synchronize with mouse movements on the screen, resulting in imitation of eyes following the cursor. The page is powered with javascript.";
+const descriptionBus = "Bus Stops project is a web page, which displays a map of Chicago city, using MapBox API. It consequently adds markers of bus stops from MIT to another University.";
 const descriptionPacman = "PacMan project is a web page, with Pacmen moving randomly on the screen, while changing animations. It is powered by javascript. Animation concept is having 2*2 two-dimensional pictures array switching between its element with setInterval function.";
 
 
